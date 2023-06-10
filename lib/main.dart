76d5e5bf03ef0a14fa0c3ca952169cd9e212b81f
AlexWalker222 <alexw3071@gmail.com>
2023-06-10 13:31:21 -0400

    ":)My third commit making the app more appealing see comments for more details on how to do extracted widget from text through refactoring changed Text to BigCard created a build method then added padding and also wrap with widget was used to create a new type called 'Card' "

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 void main() {
   runApp(MyApp());
@@ -27,19 +27,48 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current; // optimized for performance
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A random AWESOME idea:'), // Example text Change
+          BigCard(pair: pair), // Text Widget is now refactored to BigCard
+          ElevatedButton(  // Button Widget
+            onPressed: () {
+              appState.getNext();  // button action
+            },
+            child: Text('Next'), //button Text
+          ),
         ],
       ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget { // Created after refactoring Text
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding( // Wrapped text with Padding
+      padding: const EdgeInsets.all(20), // increased Padding for breathing room
+      child: Text(pair.asLowerCase),
     );
   }
 }
